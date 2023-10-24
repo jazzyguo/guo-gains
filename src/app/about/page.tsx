94b6ab48267cf76214bcdb36a8b6a0cf7152109b
Jazzy Guo <guojazzy@gmail.com>
2023-10-24 17:10:01 -0400

    feat: add dropdown nav

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import { Logo } from "@/components/ui/logo"
+
+const AboutPage = () => (
+    <div className="text-lg text-neutral-500 flex flex-col items-center max-w-screen-sm mx-auto">
+        <div className="bg-gradient-primary py-8 mb-8">
+            <Logo className="w-[200px] h-[200px]" />
+        </div>
+        <p>
+            {`At Guo Gains, we're on a mission to redefine your fitness journey. We believe that every individual has the potential to achieve their health and wellness goals, and we're here to make that journey as exciting and empowering as possible. Our story began with a passion for fitness and a desire to create a platform that caters to everyone, whether you're a seasoned athlete or just taking your first step towards a healthier lifestyle.`}
+        </p>
+        <br />
+        <p>
+            {`Our cutting-edge fitness program generator is at the heart of what we do. It's the result of countless hours of dedication and expertise, designed to tailor fitness programs that suit your unique aspirations. We know that fitness is not one-size-fits-all, which is why our approach is both intuitive and adaptable. We've combined innovative technology with a deep understanding of fitness principles to offer you a personalized experience that will keep you motivated every step of the way.`}
+        </p>
+    </div>
+)
+
+export default AboutPage
---
+++ src/components/dropdown-nav/index.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import Link from "next/link"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+export const DropdownNav = () => (
+    <>
+        <div className="block md:hidden">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>☰</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <Link href="/about" legacyBehavior passHref>
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    About
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+        <div className="hidden md:block">
+            <Link href="/about" className="hover:bg-neutral-200 p-4">
+                <span className="font-bold text-xl">About</span>
+            </Link>
+        </div>
+    </>
+)
