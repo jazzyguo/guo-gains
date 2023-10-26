5b60b68fd769c569563c5c61ac3e75004ecb2b3b
Jazzy Guo <guojazzy@gmail.com>
2023-10-26 19:09:26 -0400

    feat: add zod validation to  form schema

--- src/features/steps/store/index.ts
+++ src/features/steps/store/index.ts
@@ -2,8 +2,15 @@ import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { getImperialHeightFromMetric, convertWeight } from "@/lib/unitsConvert";
+import { z } from "zod";
 
 export const NUMBER_STEPS = 2;
+
+const BASE_HEIGHT_CM = 175;
+const BASE_WEIGHT_KG = 70;
+export const [BASE_HEIGHT_FT, BASE_HEIGHT_INCHES] =
+  getImperialHeightFromMetric(BASE_HEIGHT_CM);
+export const BASE_WEIGHT_LBS = convertWeight(BASE_WEIGHT_KG, "metric");
 
 // tracks flow of the steps form
 // as well as the form data across steps
@@ -11,19 +18,21 @@ interface StepsState {
   latestStep: number;
 }
 
-export interface FormState {
-  age: number;
-  gender: "male" | "female" | null;
-  height_cm: number;
-  height_ft: number;
-  height_inches: number;
-  weight_kg: number;
-  weight_lbs: number;
-  body_fat_range: [number, number] | null;
-  fitness_goal: string | null;
-  target_workout_days: number;
-  current_activity_level: string | null;
-}
+const FormSchema = z.object({
+  age: z.number().default(20),
+  gender: z.enum(["male", "female"]).nullable().default(null),
+  height_cm: z.number().default(BASE_HEIGHT_CM),
+  height_ft: z.number().default(BASE_HEIGHT_FT),
+  height_inches: z.number().default(BASE_HEIGHT_INCHES),
+  weight_kg: z.number().default(BASE_WEIGHT_KG),
+  weight_lbs: z.number().default(BASE_WEIGHT_LBS),
+  body_fat_range: z.tuple([z.number(), z.number()]).nullable().default(null),
+  fitness_goal: z.string().nullable().default(null),
+  target_workout_days: z.number().default(3),
+  current_activity_level: z.string().nullable().default(null),
+});
+
+type FormState = z.infer<typeof FormSchema>;
 
 type Actions = {
   setLatestStep: (step: number) => void;
@@ -34,25 +43,7 @@ type Actions = {
   reset: () => void;
 };
 
-const BASE_HEIGHT_CM = 175;
-const BASE_WEIGHT_KG = 70;
-export const [BASE_HEIGHT_FT, BASE_HEIGHT_INCHES] =
-  getImperialHeightFromMetric(BASE_HEIGHT_CM);
-export const BASE_WEIGHT_LBS = convertWeight(BASE_WEIGHT_KG, "metric");
-
-const initialFormData: FormState = {
-  age: 20,
-  gender: null,
-  height_cm: BASE_HEIGHT_CM,
-  height_ft: BASE_HEIGHT_FT,
-  height_inches: BASE_HEIGHT_INCHES,
-  weight_kg: BASE_WEIGHT_KG,
-  weight_lbs: BASE_WEIGHT_LBS,
-  body_fat_range: null,
-  fitness_goal: null,
-  target_workout_days: 3,
-  current_activity_level: null,
-};
+const initialFormData: FormState = FormSchema.parse({});
 
 const initialState: StepsState & FormState = {
   latestStep: 1,
