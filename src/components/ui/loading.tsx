ae79a31b7624c019c872f36e1e437d1cca565faa
Jazzy Guo <guojazzy@gmail.com>
2023-10-26 14:05:38 -0400

    feat: add lazy loader indiciator

---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,9 @@
+export const Loading = () => (
+    <div className="flex justify-center items-center pt-[10rem]">
+        <div className="relative inline-flex">
+            <div className="w-5 h-5 bg-violet-500 rounded-full"></div>
+            <div className="w-5 h-5 bg-violet-500 rounded-full absolute top-0 left-0 animate-ping"></div>
+            <div className="w-5 h-5 bg-violet-500 rounded-full absolute top-0 left-0 animate-pulse"></div>
+        </div>
+    </div>
+)
