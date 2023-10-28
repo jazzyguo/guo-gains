f58813446649f10048c40c086537ace6d844e3fe
Jazzy Guo <guojazzy@gmail.com>
2023-10-27 22:33:03 -0400

    fix: mobile header z:

--- src/features/steps/components/mobile-header.tsx
+++ src/features/steps/components/mobile-header.tsx
@@ -8,7 +8,7 @@ type Props = {
 // StepsPath is rendered on top for mobile, and inside the nav footer on desktop
 export const MobileHeader = ({ currentStep, latestStep }: Props) => (
     <div
-        className="sticky top-0 md:hidden w-full bg-neutral-100 border-t border-gray-200 z-10 py-4 h-[80px]"
+        className="sticky top-0 md:hidden w-full bg-neutral-100 border-t border-gray-200 z-[60] py-4 h-[80px]"
     >
         <StepsPath
             currentStep={currentStep}
